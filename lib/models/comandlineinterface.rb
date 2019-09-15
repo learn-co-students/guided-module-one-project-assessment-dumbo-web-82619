4f93477cd59928b73742ad5f9cdc3151c8b51c81
mykle jones <mykle.j91@gmail.com>
2019-09-15 16:08:19 -0400

    cfa

--- lib/models/comandlineinterface.rb
+++ lib/models/comandlineinterface.rb
@@ -1,79 +1,247 @@
 class ComandLineInterface 
 
-    attr_accessor :prompt, :user
-def initialize()
-    @prompt = TTY::Prompt.new
-end
-
-def greet
-    system "clear"
-    puts"**********************Welcome to  C.F.A************************"
- 
-    # choice = self.prompt.select("Are you a ...") do |m|
-    #     m.choice "CONTRACTOR"
-    #     m.choice "FREELANCER"
-    # end
-    # case choice
-    # when "CONTRACTOR"
-       puts "Welcome Contractor"
-       choice = self.prompt.select("Are you a ....") do |m|
-        m.choice "new user",->{Contractor.new_contractor}
-        m.choice "Existing_user",->{Contractor.verify_contractor}
-       end
-       @user = choice
-       while @user ==nil
+    attr_accessor :prompt, :user, :pastel
+    
+    def initialize()
+    @prompt = TTY::Prompt.new(symbols: {tick: '√'}, active_color: :cyan)
+    @pastel = Pastel.new
+    end
+    
+    def greet
         system "clear"
-                puts "
-                
-                
-                
-                
-                
-                
-                
-                
-                *****👀👀👀[CONTRACTOR NOT FOUND]👀👀👀*****
-                "
+       
+        puts pastel.decorate("**********************Welcome to  C.F.A************************", :bright_green)
+        
+        self.art_work
+        sleep 3
+        
+    
+        # choice = self.prompt.select("Are you a ...") do |m|
+        #     m.choice "CONTRACTOR"
+        #     m.choice "FREELANCER"
+        # end
+        # case choice
+        # when "CONTRACTOR"
+        puts "Hello Contractor"
+        choice = self.prompt.select("Are you a ....") do |m|
+            m.choice "new user",->{Contractor.new_contractor}
+            m.choice "Existing_user",->{Contractor.verify_contractor}
+        end
+        @user = choice
+        while @user ==nil
+            system "clear"
+                    puts "
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    *****👀👀👀[CONTRACTOR NOT FOUND]👀👀👀*****
+                    "
 
-            sleep 3
-            greet
-       end
-       cont_menu(@user) 
-    # when "FREELANCER"
-    #     puts "Welcome Freelancer"
-    #     choice = self.prompt.select("Are you a ....") do |m| 
-    #      m.choice "new user",->{Freelancer.new_freelancer}
-    #      m.choice "Existing_user",->{Freelancer.verify_freelancer}
-    #     end
-    #     @user = choice
-    #     fl_menu 
+                    sleep 3
+                    greet
+        end
+        cont_menu(@user) 
+        # when "FREELANCER"
+        #     puts "Welcome Freelancer"
+        #     choice = self.prompt.select("Are you a ....") do |m| 
+        #      m.choice "new user",->{Freelancer.new_freelancer}
+        #      m.choice "Existing_user",->{Freelancer.verify_freelancer}
+        #     end
+        #     @user = choice
+        #     fl_menu 
+        # end               
+    end
+    
+    
+    
+    
+    def cont_menu(user_instance)
+        system "clear"
+        choice = prompt.select("What would you like to do today") do |m|
+            m.choice "view freelancers",->{user_instance.my_freelancers(self)}
+            m.choice "View contracts",->{user_instance.my_contracts(self)}
+            m.choice "[LOG_OUT]",->{exit_cont_menu}
+        end
+        
+    end
+    
+    
+    # def self.cont_menu(user_instance)
+    #     cont_menu(user_instance)
     # end
-
-
-end
-
- def cont_menu(user_instance)
-    system "clear"
-    choice = prompt.select("What would you like to do today") do |m|
-        m.choice "view freelancer",->{user_instance.my_freelancers(self)}
-        m.choice "View contracts",->{user_instance.my_contracts(self)}
-        m.choice "[LOG_OUT]",->{}
-   end
-   
-end
-
-
-# def fl_menu
-#     system "clear"
-#     choice = self.prompt.select("What would you like to do today") do |m|
-#         m.choice "Update your bio ?",->{user_instance.update_bio(self)}
-#         m.choice "View your contracts ?",->{self.user.my_contracts(self) }
-#         m.choice "[LOG_OUT]",->{}
+    # def fl_menu
+    #     system "clear"
+    #     choice = self.prompt.select("What would you like to do today") do |m|
+    #         m.choice "Update your bio ?",->{user_instance.update_bio(self)}
+    #         m.choice "View your contracts ?",->{self.user.my_contracts(self) }
+    #         m.choice "[LOG_OUT]",->{}
 #    end
 # end
 
+def exit_cont_menu
+    puts "🔚"
+    sleep 1
+    puts "🔚🔚🔚🔚" 
+    sleep 1
+    puts "🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚"
+    sleep 1
+    puts "🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚"
+    sleep 1
+    puts "🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚"
+    puts "      
+    
+
+    
+
+    
+
+    
+    
+**********[GOOD BYE]**********
 
 
 
 
+
+
+
+
+"
+exit
 end
+
+def self.exit_cont_menu
+    system "clear"
+    puts "🔚"
+    sleep 1
+    puts "🔚🔚🔚🔚" 
+    sleep 1
+    puts "🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚"
+    sleep 1
+    puts "🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚"
+    sleep 1
+    puts "🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚"
+    puts "      
+    
+
+    
+    
+    
+    
+
+    
+**********[GOOD BYE]**********
+
+
+
+
+
+
+
+
+"
+exit
+end
+
+
+
+
+#     def art_work
+#         art = puts "[
+#                                    _
+#                                  ,d8b,
+#                          _,,aadd8888888bbaa,,_
+#                     _,ad88P"""8,  I8I  ,8"""Y88ba,_
+#                  ,ad88P" `Ya  `8, `8' ,8'  aP' "Y88ba,
+#                ,d8"' "Yb   "b, `b  8  d' ,d"   dP" `"8b,
+#               dP"Yb,  `Yb,  `8, 8  8  8 ,8'  ,dP'  ,dP"Yb
+#            ,ad8b, `Yb,  "Ya  `b Y, 8 ,P d'  aP"  ,dP' ,d8ba,
+#           dP" `Y8b, `Yb, `Yb, Y,`8 8 8',P ,dP' ,dP' ,d8P' "Yb
+#          ,88888888Yb, `Yb,`Yb,`8 8 8 8 8',dP',dP' ,dY88888888,
+#          dP     `Yb`Yb, Yb,`8b 8 8 8 8 8 d8',dP ,dP'dP'     Yb
+#         ,8888888888b "8, Yba888888888888888adP ,8" d8888888888,
+#         dP        `Yb,`Y8P""'             `""Y8P',dP'        Yb
+#        ,88888888888P"Y8P'_.---.._     _..---._`Y8P"Y88888888888,
+#        dP         d'  8 '  ____  `. .'  ____  ` 8  `b         Yb
+#       ,888888888888   8   <(@@)>  | |  <(@@)>   8   888888888888,
+#       dP          8   8    `"""         """'    8   8          Yb
+#      ,8888888888888,  8          ,   ,          8  ,8888888888888,
+#      dP           `b  8,        (.-_-.)        ,8  d'           Yb
+#     ,88888888888888Yaa8b      .'       `.      d8aaP88888888888888,
+#     dP               ""8b     _,gd888bg,_     d8""               Yb
+#    ,888888888888888888888b,    ""Y888P""    ,d888888888888888888888,
+#    dP                   "8"b,             ,d"8"                   Yb
+#   ,888888888888888888888888,"Ya,_,ggg,_,aP",888888888888888888888888,
+#   dP                      "8,  "8"\x/"8"  ,8"                      Yb
+#  ,88888888888888888888888888b   8\\x//8   d88888888888888888888888888,
+#  8888bgg,_                  8   8\\x//8   8                  _,ggd8888
+#   `"Yb, ""8888888888888888888   Y\\x//P   8888888888888888888"" ,dP"'
+#     _d8bg,_"8,              8   `b\x/d'   8              ,8"_,gd8b_
+#   ,iP"   "Yb,8888888888888888    8\x/8    8888888888888888,dP"  `"Yi,
+#  ,P"    __,888              8    8\x/8    8              888,__    "Y,
+# ,8baaad8P"":Y8888888888888888 aaa8\x/8aaa 8888888888888888P:""Y8baaad8,
+# dP"':::::::::8              8 8::8\x/8::8 8              8:::::::::`"Yb
+# 8::::::::::::8888888888888888 8::88888::8 8888888888888888::::::::::::8
+# 8::::::::::::8,             8 8:::::::::8 8             ,8::::::::::::8
+# 8::::::::::::8888888888888888 8:::::::::8 8888888888888888::::::::::::8
+# 8::::::::::::Ya             8 8:::::::::8 8             aP::::::::::::8
+# 8:::::::::::::888888888888888 8:::::::::8 888888888888888:::::::::::::8
+# 8:::::::::::::Ya            8 8:::::::::8 8            aP:::::::::::::8
+# Ya:::::::::::::88888888888888 8:::::::::8 88888888888888:::::::::::::aP
+# `8;:::::::::::::Ya,         8 8:::::::::8 8         ,aP:::::::::::::;8'
+#  Ya:::::::::::::::"Y888888888 8:::::::::8 888888888P":::::::::::::::aP
+#  `8;::::::::::::::::::::""""Y8888888888888P""""::::::::::::::::::::;8'
+#   Ya:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::aP
+#    "b;::::::::::::::::::::::::::::::::::::::::::: Normand  ::::::;d"
+#     `Ya;::::::::::::::::::::::::::::::::::::::::: Veilleux ::::;aP'
+#       `Ya;:::::::::::::::::::::::::::::::::::::::::::::::::::;aP'
+#          "Ya;:::::::::::::::::::::::::::::::::::::::::::::;aP"
+#             "Yba;;;:::::::::::::::::::::::::::::::::;;;adP"
+#                 `"""""""Y888888888888888888888P"""""""'
+
+#     ]"
+# end 
+    
+def art_work
+art= puts <<-"EOF"
+           '
+            *          .
+                   *       '
+              *                *
+
+
+
+
+
+   *   '*
+           *
+                *
+                       *
+               *
+                     *
+
+         .                      .
+         .                      ;
+         :                  - --+- -
+         !           .          !
+         |        .             .
+         |_         +
+      ,  | `.
+--- --+-<#>-+- ---  --  -
+      `._|_,'
+         T
+         |
+         !
+         :         . : 
+         .       *
+
+
+ EOF
+art
+end
+ end
--- lib/models/contractor.rb
+++ lib/models/contractor.rb
@@ -3,11 +3,11 @@ class Contractor < ActiveRecord::Base
     has_many :freelancers, through: :contracts
 
     def self.tty_prompt
-        prompt = TTY::Prompt.new
+        prompt = TTY::Prompt.new(symbols: {tick: '√'})
     end
 
     def tty_prompt
-        prompt = TTY::Prompt.new
+        prompt = TTY::Prompt.new(symbols: {tick: '√'})
     end
 
 
@@ -24,45 +24,22 @@ class Contractor < ActiveRecord::Base
             yes = Contractor.all.find_by(name: name)
     end 
            
-    def new_contract(user_instance)
-            description = tty_prompt.ask("description:")
-            pay_out = tty_prompt.ask("PayOut")
-            start_date = tty_prompt.ask("StartDate")
-            choice = tty_prompt.select("Choose Freelancer", Freelancer.all.map{|person| person.name}) 
-            freelancer_id = Freelancer.all.find_by(name: choice)
-            Contract.create(contractor_id: user_instance, freelancer_id: freelancer_id.id, description: description, pay_out: pay_out, start_date: start_date )
-            system "clear"
-            puts "
-            
-            
-            
-            
-                                    *****CONTRACT CREATED*****
-            
-            
-            
-            
-            "
-            sleep 3
-            var = user_instance
-        var
-    end
-           
+    
     def new_freelancer(user_instance)
-            name = tty_prompt.ask("what is there name?")
-            age = tty_prompt.ask("what is there age?")
-            
-            dob = tty_prompt.ask("what is there date of birth?")
-            certifications = tty_prompt.ask("Any particular feild cerification? ")
+        system "clear"
+        name = tty_prompt.ask("what is there name?")
+        age = tty_prompt.ask("what is there age?")
+        
+        dob = tty_prompt.ask("what is there date of birth?")
+        certifications = tty_prompt.ask("Any particular feild cerification? ")
             Freelancer.create(name: name, age: age, dob: dob, certifications: certifications)  
             system "clear"
             puts "*****Freelancer_added*****" 
             sleep 3
-            var = user_instance
-        var
-    end
+            finder(user_instance)
+        end
 
-    def freelance_name
+        def freelance_name
         Freelancer.all.map{|n| n.name}
     end
 
@@ -74,21 +51,36 @@ class Contractor < ActiveRecord::Base
         end
         case fr_option
         when "Select a existing FREELANCER"
+            system "clear"
         fl_name = tty_prompt.select( "What freelancer would you like to view", freelance_name )
             choice = tty_prompt.select("What would you like to do with #{fl_name} ?") do |m|
                 m.choice "Update Freelancer",->{fl_update(fl_name, user_instance)}
                 m.choice "Remove Freelancer",->{remove_freelance(fl_name, user_instance)}
+                m.choice "View Freelancer",->{view_freelancer(fl_name, user_instance)}
                 m.choice "back to menu"
             end 
             case choice
             when "back to menu"
-                var = user_instance
-                var
+                finder(user_instance)
             end
         end 
     end
+    
+    def view_freelancer(fl_name,user_instance)
+        obj = Freelancer.all.find_by(name: fl_name)
+        puts "#{obj.name}"
+        puts "#{obj.age}"
+        puts "#{obj.dob}"
+        puts "#{obj.certifications}"
+
+         sleep 3
+         finder(user_instance)
+         
+         # name_inst = Freelancer.all.select{|m| m.name == fl_name}
+    end
 
     def fl_update(fl_name,user_instance)
+        puts "Updating #{fl_name}"
         name = tty_prompt.ask("NEW name:")
         age = tty_prompt.ask("NEW age:")
         dob = tty_prompt.ask("NEW dob:")
@@ -96,19 +88,19 @@ class Contractor < ActiveRecord::Base
         Freelancer.all.find_by(name: fl_name).update(name: name, age: age, dob: dob, certifications: certifications)
         system 'clear'
         puts "
-
+        
                               *****📞📞📞📞📞LANCER HAS BEEN UPDATED📞📞📞📞📞*****
+                              
                                                 
+                              
                                                 
-                                                
-                                                
-        "
+                              "
         sleep 3
-        var = user_instance
-        var
+        finder(user_instance)
     end
     
     def remove_freelance(fl_name,user_instance)
+        system "clear"
         tty_prompt.select("THIS WILL PERMANENTLY DELETE FREELANCER") do |m| 
         m.choice "yes",-> {Freelancer.all.find_by(name: fl_name).delete}
         m.choice "no",->{user_instance.cont_menu}
@@ -123,15 +115,14 @@ class Contractor < ActiveRecord::Base
                             *****🗑🗑🗑🗑🗑🗑🗑[LANCER HAS BEEN REMOVED]🗑🗑🗑🗑🗑🗑🗑*****
                                                                             
                                                                             
+                            
                                                                             
                                                                             
                                                                             
-                                                                            
-                                                                            
+                            
         "
         sleep 3
-        var = user_instance
-        var
+        finder(user_instance)
     end 
     
     def contract_name
@@ -141,12 +132,16 @@ class Contractor < ActiveRecord::Base
     
     
     def my_contracts(user_instance)
+        system "clear"
         cont_option = tty_prompt.select("Would you lke to...") do |m|
-            m.choice "Select a existing CONTRACT"
-            m.choice "Make new 'CONTRACT'",->{new_contract(user_instance)}
+            m.choice "  🗃 🗃 🗃 🗃 Select a existing CONTRACT  🗃 🗃 🗃 🗃    "
+            m.choice "     📝  📝  📝   [Make new 'CONTRACT]    📝  📝  📝     ",->{new_contract(user_instance)}
+            m.choice "        🛰🛰🛰       [FREELANCERS]       🛰🛰🛰          ",->{my_freelancers(user_instance)}
+            m.choice "                         [EXIT]                            ",->{  ComandLineInterface.exit_cont_menu}
         end
         case cont_option
-        when "Select a existing CONTRACT"
+        when "  🗃 🗃 🗃 🗃 Select a existing CONTRACT  🗃 🗃 🗃 🗃"
+            system "clear"
             cont_name = tty_prompt.select( "What CONTRACT would you like to view", contract_name )
             choice = tty_prompt.select("What would you like to do with #{cont_name} ?") do |m|
                 m.choice "UPDATE CONTRACT",->{cont_update(cont_name, user_instance)}
@@ -155,18 +150,45 @@ class Contractor < ActiveRecord::Base
             end
             case choice
             when "back to menu"
-                var = user_instance
-                var
+                
             end  
         end
+        finder(user_instance)
     end
     
-    def finder
-        
+    def new_contract(user_instance)
+
+        system "clear"
+            description = tty_prompt.ask("description:")
+            pay_out = tty_prompt.ask("PayOut")
+            start_date = tty_prompt.ask("StartDate")
+            choice = tty_prompt.select("Choose Freelancer", Freelancer.all.map{|person| person.name}) 
+            freelancer_id = Freelancer.all.find_by(name: choice)
+            Contract.create(contractor_id: user_instance, freelancer_id: freelancer_id.id, description: description, pay_out: pay_out, start_date: start_date )
+            system "clear"
+            puts "
+            
+            
+            
+            
+               x***** 📄 📄 📄 📄 📄 👀👀👀👀👀👀 [CONTRACT CREATED] 📄 📄 📄 📄 📄 👀👀👀👀👀👀 *****
+            
+            
+            
+            
+            "
+            sleep 3
+            finder(user_instance)
+    end
+
+    def finder(user_instance)
+        my_contracts(user_instance)
     end
 
 
     def cont_update(cont_name, user_instance)
+        system "clear"
+        puts "TYPE.."
         # cont_description = tty_prompt.select("Which CONTRACT", Contract.all.map{|c| c.description})
         description = tty_prompt.ask("NEW description:")
         freelance_ = tty_prompt.select("Choose Freelancer", Freelancer.all.map{|person| person.name}) 
@@ -179,17 +201,17 @@ class Contractor < ActiveRecord::Base
         
         
         
-                                *****✒️✒️✒️✒️✒️CONTRACTOR HAS BEEN UPDATED🖋🖋🖋🖋🖋*****
+                             *****✒️✒️✒️✒️✒️  [CONTRACT HAS BEEN UPDATED]  🖋🖋🖋🖋🖋*****
         
         
         
         "
         sleep 3
-        var = user_instance
-        var
+        finder(user_instance)
     end
     
     def remove_cont(cont_name,user_instance)
+        system "clear"
         tty_prompt.select("THIS WILL PERMANENTLY DELETE CONTRACT") do |m| 
             m.choice "yes",-> {Contract.all.find_by(description: cont_name).delete}
             m.choice "no",->{user_instance.cont_menu}
@@ -203,8 +225,8 @@ class Contractor < ActiveRecord::Base
 
         
         
-                            *****🗑🗑🗑🗑🗑🗑CONTRACT HAS BEEN DELETED🗑🗑🗑🗑🗑🗑*****
-        
+                            *****🗑 🗑 🗑 🗑 🗑 🗑 [CONTRACT HAS BEEN DELETED]  🗑 🗑 🗑 🗑 🗑 🗑*****
+         
         
         
         
@@ -213,8 +235,21 @@ class Contractor < ActiveRecord::Base
         
         "
         sleep 3
-        var = user_instance
-        var
+        finder(user_instance)
     end
         
+
+    def view_cont(cont_name,user_instance)
+        obj = Cotract.all.find_by(description: cont_name)
+        puts "#{obj.decription}"
+        puts "#{obj.pay_out}"
+        puts "#{obj.start_date}"
+        # puts "#{obj.certifications}"
+
+         sleep 3
+         finder(user_instance)
+         
+         # name_inst = Freelancer.all.select{|m| m.name == fl_name}
+    end
+
 end
